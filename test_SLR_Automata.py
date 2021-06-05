ea657b0fe1bf216ed8ba80a0943bb4873eceb228
M010K <1223987303@qq.com>
2021-06-05 14:51:46 +0800

    update: change file to grammar.txt and input.txt; add test for slr table

--- test_SLR_Automata.py
+++ test_SLR_Automata.py
@@ -4,6 +4,7 @@ from Grammar import Grammar
 from Scanner import Scanner
 from SLR_Automata import SLR_Automata
 from Symbol_Table import Symbol_Table
+from SLR_Table import SLR_Table, print_slr_table
 
 console = Console()
 
@@ -12,11 +13,17 @@ if __name__ == "__main__":
 
     # init grammar
     grammar = Grammar()
-    grammar.read("grammar_test.txt")
+    grammar.read("grammar.txt")
 
     # init scanner
-    with open("input_test.txt", "r") as f:
+    with open("input.txt", "r") as f:
         scanner = Scanner(f.read(), symbol_table)
+
+    # generate and print slr table
+    slr = SLR_Table(grammar)
+    slr.analysis_table()
+
+    print_slr_table(grammar)
 
     # init slr automata
     slr = SLR_Automata(scanner, grammar)
